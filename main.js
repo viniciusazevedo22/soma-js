675f828a3be923dbe2bee8c070d70422a20c6700
viniciusazevedo22 <163418874+viniciusazevedo22@users.noreply.github.com>
2024-11-07 09:58:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let numero1 = parseInt(prompt("informe o número 1 : "));
+let numero2 = parseInt(prompt("informe o número 2 : "));
+
+let soma = numero1 + numero2;
+
+document.write("a soma é igual a: ", soma);
+console.log ("o valor da soma no console é igual a: ", soma);
